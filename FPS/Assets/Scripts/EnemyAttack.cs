083228a7f611c072e1346c9c7cd984d7f364d89a
cg <cgrimaldi19@apu.edu>
2022-05-01 22:52:50 -0700

    scripts

--- FPS/Assets/Scripts/EnemyAttack.cs
+++ FPS/Assets/Scripts/EnemyAttack.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;public class EnemyAttack : MonoBehaviour
 {
     PlayerHealthScript Target;
-    [SerializeField] Transform target;
+   [SerializeField] Transform target;
     [SerializeField] float damage = 40f;
     // Start is called before the first frame update
     void Start()
@@ -17,6 +17,7 @@ using UnityEngine;public class EnemyAttack : MonoBehaviour
             return;
         }
         print("player has been hit.");
+        target.GetComponent<DisplayDamage>().ShowDamageImpact();
         target.GetComponent<PlayerHealthScript>().TakeDamage(damage);
     }    // Update is called once per frame
 }
